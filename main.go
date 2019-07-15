021bf5fdf3a44fb9dbb2d23a4858b4b573ef72ae
bzdvdn <bzdv.dn@gmail.com>
2019-07-15 13:22:17 +0300

    basic 404 handler

--- main.go
+++ main.go
@@ -17,6 +17,17 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get information, please send an email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "faq")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "404")
+}
+
 // func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 // 	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
 // }
@@ -30,6 +41,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", home)
 	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
 	// http.HandleFunc("/", handlerFunc)
 	// gmux.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", router) // if you have router or mux, you need to include this
